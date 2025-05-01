511fb1f6a6f5711980b38ae1ba818213ddd56b74
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-05-01 17:11:54 +0530

    utils: configure

--- src/utils/configure.rs
+++ src/utils/configure.rs
@@ -4,14 +4,72 @@ use colored::Colorize;
 pub fn install_linker(linker: &str) {
     match OS {
         "windows" => {
-
+            if linker.contains("lld") {
+                println!("[LOG]: lld {}", "enabled".bright_green());
+            }
         }
         "linux" => {
+            if linker.contains("mold") {
+                println!("[LOG]: mold {}", "enabled".bright_green());
+            }
+
+            if linker.contains("lld") {
+                println!(
+                    "{} {}",
+                    "$".bright_black(),
+                    "sudo apt install clang lld".bright_yellow()
+                );
+
+                println!("{:?}", sys_info::linux_os_release());
+
+                let result = std::process::Command::new("bash")
+                    .arg("-c")
+                    .arg("sudo apt install clang lld")
+                    .status()
+                    .expect("Failed to install clang + lld");
+
+                if result.success() {
+                    println!("[LOG]: lld {}", "enabled".bright_green());
+                }
+            }
         }
         "macos" => {
+            if linker.contains("zld") {
+                println!(
+                    "{} {}",
+                    "$".bright_black(),
+                    "brew install michaeleisel/zld/zld".bright_yellow()
+                );
 
+                let result = std::process::Command::new("bash")
+                    .arg("-c")
+                    .arg("brew install michaeleisel/zld/zld")
+                    .status()
+                    .unwrap();
+
+                if result.success() {
+                    println!("[LOG]: zld {}", "enabled".bright_green());
+                }
+            }
+
+            if linker.contains("lld") {
+                println!(
+                    "{} {}",
+                    "$".bright_black(),
+                    "sudo apt install clang lld".bright_yellow()
+                );
+
+                let result = std::process::Command::new("bash")
+                    .arg("-c")
+                    .arg("sudo apt install clang lld")
+                    .status()
+                    .unwrap();
+
+                if result.success() {
+                    println!("[LOG]: lld {}", "enabled".bright_green());
+                }
+            }
         }
-
         &_ => {}
     }
 }
